007820363bf1e373d672cf804f9ffa4007afe8da
廖述峰 <shufeng.lsf@alibaba-inc.com>
2017-04-28 11:37:50 +0800

    精簡腳本

--- recorder.py
+++ recorder.py
@@ -1,24 +1,19 @@
 # coding:utf-8
 import os
-import threading
-import sys
+
 
 import subprocess
 
-import time
+import re
 
-from eventparser import EventParser
-from watcher import monitor_change
+import sys
 
-origin_event_file = os.path.join(os.getcwd(), "events.txt")
-parser_event_file = os.path.join(os.getcwd(), "events.sh")
+origin_event_file = os.path.join(os.getcwd(), "events.sh")
 
 STOP_FLAG = False
 PID = None
 
-
-def record_daemon():
-    shell_command("/home/alex/Android/Sdk/platform-tools/adb shell getevent -t > " + origin_event_file)
+EVENT_LINE_RE = re.compile(r"\[.+?(\d+)\.(\d+)\] (\S+): (\S+) (\S+) (\S+)")
 
 
 def shell_command(cmd, result=False):
@@ -34,50 +29,54 @@ def shell_command(cmd, result=False):
             return buff
 
 
+def record(fpath, t_fix=0.1, last_time=None, eventNum=None):
+    record_command = "adb shell getevent -t"
+    adb = subprocess.Popen(record_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    outputFile = open(fpath, 'w')
+    outputFile.write("#!/bin/sh\n")
+    while adb.poll() is None:
+        try:
+            line = adb.stdout.readline().decode('utf-8', 'replace').strip()
+            match = EVENT_LINE_RE.match(line.strip())
+            if match is not None:
+                tsec, tmsec, dev, etype, ecode, data = match.groups()
+                cur_time = float(tsec) + float(tmsec[:2]) / 100
+
+                if last_time is not None:
+                    diff_time = (cur_time - last_time)
+                    if diff_time > 0.2:
+                        outputFile.write('sleep %.2f\n' % (diff_time - t_fix,))
+                last_time = cur_time
+
+                if eventNum is not None and '/dev/input/event%s' % (eventNum) != dev:
+                    continue
+                etype, ecode, data = int(etype, 16), int(ecode, 16), int(data, 16)
+                outputFile.write("sendevent %s %s %s %s\n" % (dev, etype, ecode, data))
+        except KeyboardInterrupt:
+            break
+        if len(line) == 0:
+            break
+    outputFile.close()
+
+
+def play():
+    shell_command("adb push events.sh /data/local/tmp/")
+    shell_command("adb shell chmod 755 /data/local/tmp/events.sh")
+    shell_command("adb shell sh /data/local/tmp/events.sh")
+
+
 def prepare():
     if os.path.isfile(origin_event_file):
         os.remove(origin_event_file)
-    if os.path.isfile(parser_event_file):
-        os.remove(parser_event_file)
     shell_command("adb logcat -c")
-    # shell_command("adb logcat -d > log/install.log")
     print "clear all log file."
 
 
-def main():
-    if sys.argv[1] == "start":
-        prepare()
-        t = threading.Thread(target=record_daemon, args=())
-        t.setDaemon(True)
-        t.start()
-        time.sleep(3)
-        start = time.time()
-        monitor_change(origin_event_file)  # block job
-        print "cost " + str(time.time() - start)
-        if PID is not None:
-            os.killpg(PID, 9)
-        
-        # parser result to sh scripts
-        event_parser = EventParser(origin_event_file)
-        event_parser.get_events().save(parser_event_file)
-
-    elif sys.argv[1] == "play":
-        shell_command("/usr/bin/sh " + parser_event_file)
-
-
 if __name__ == '__main__':
-    prepare()
-    t = threading.Thread(target=record_daemon, args=())
-    t.setDaemon(True)
-    t.start()
-    time.sleep(2)
-    start = time.time()
-    monitor_change(origin_event_file)  # block job
-    print "cost " + str(time.time() - start)
-    if PID is not None:
-        os.kill(PID, 9)
-        shell_command('ps -ef | grep "getevent" | grep -v "grep" | cut -c 9-15 | xargs kill -9')
-
-    # parser result to sh scripts
-    event_parser = EventParser(origin_event_file)
-    event_parser.get_events().save(parser_event_file)
+    if sys.argv[1] == "record":
+        prepare()
+        record(origin_event_file)
+    elif sys.argv[1] == "play":
+        shell_command("adb push events.sh /data/local/tmp/")
+        shell_command("adb shell chmod 755 /data/local/tmp/events.sh")
+        shell_command("adb shell sh /data/local/tmp/events.sh")
